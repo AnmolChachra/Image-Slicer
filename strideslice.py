b54f771c68f156ef10796b0b1a05e78c5a0c3432
AnmolChachra <anmol.chachra@gmail.com>
2018-12-08 23:42:29 +0530

    Update strideslice.py

--- strideslice.py
+++ strideslice.py
@@ -1,162 +1,140 @@
 import numpy as np
-import cv2
+import matplotlib.pyplot as plt
 import os
  
-def read_images(dir_path):
-    Images = []
-    image_names = sorted(os.listdir(dir_path))
-    print(image_names)
-    for im in image_names:
-        image = cv2.imread(os.path.join(dir_path,im))
-        Images.append(image)
-    return Images
-
-def image_size(image):
-    return image.shape
-
-def save_images(transformed, save_dir):
-    if not(os.path.exists(save_dir)):
-        raise Exception("Path does not exist!")
-    else:
-        for key, val in transformed.items():
-            path = os.path.join(save_dir, key)
-            os.mkdir(path)
-            for i, j in enumerate(val):
-                cv2.imwrite(os.path.join(path, str(i+1)+'.png'), j)
+class ImageSlicer(object):
+    
+    def __init__(self, source, size, strides=[None, None], BATCH = False, PADDING=False):
+        self.source = source
+        self.size = size
+        self.strides = strides
+        self.BATCH = BATCH
+        self.PADDING = PADDING
         
+    def __read_images(self):
+        Images = []
+        image_names = sorted(os.listdir(self.source))
+        for im in image_names:
+            image = plt.imread(os.path.join(dir_path,im))
+            Images.append(image)
+        return Images
 
-# Takes input(height, width), output(height, width) and strides
-# Returns offset, i.e. left out portion after applying strides
-def Offset_op(input_length, output_length, stride):
-    offset = (input_length) - (stride*((input_length - output_length)//stride)+output_length)    
-    return offset
-
-# Takes an image, offset required to fit output image dimensions with given strides and calculates the padding it needs for perfect fit
-# Return Padded image
-def Padding_op(Image, strides, offset_x, offset_y):  
-    padding_x = strides[0] - offset_x
-    padding_y = strides[1] - offset_y
-    #print(padding_x,padding_y)
-    Padded_Image = np.zeros(shape=(Image.shape[0]+padding_x, Image.shape[1]+padding_y, Image.shape[2]),dtype=Image.dtype)
-    Padded_Image[padding_x//2:(padding_x//2)+(Image.shape[0]),padding_y//2:(padding_y//2)+Image.shape[1],:] = Image    
-    return Padded_Image
-
-# Takes an image, Dimensions of the desired image and Strides
-# Returns List of cropped images
-def Convolution_op(Image, size, strides):
-    start_x = 0
-    start_y = 0
-    end_x = Image.shape[0] - size[0]
-    end_y = Image.shape[1] - size[1]
-
-    n_rows = Image.shape[0]//strides[0]
-    n_columns = Image.shape[1]//strides[1]
-
-    small_images = []
-    for i in range(n_rows-1):
-        for j in range(n_columns-1):
-            new_start_x = start_x+i*strides[0]
-            new_start_y= start_y+j*strides[1]
-            small_images.append(Image[new_start_x:new_start_x+size[0],new_start_y:new_start_y+size[1],:])
-        
-    return small_images
-
-# Transforms the images/image into desired small images provided the strides
-# If no strides are provided, the strides will default to the size of the desired image, i.e no overlapping will take place.
-# Returns the dictionary with string of count starting from 1 as key and list of images as values
-def transform(source_dir, size, strides=[None, None], PADDING=False):
-    if not(os.path.exists(source_dir)):
-        raise Exception("Path does not exist!")
-    else:
-        im_path = None
-        dir_path = None
-        splits = source_dir.split('/')
-        last = splits[-1].split('.')
-        if len(last) > 1:
-            im_path=  source_dir
+    def __offset_op(self, input_length, output_length, stride):
+        offset = (input_length) - (stride*((input_length - output_length)//stride)+output_length)
+        return offset
+    
+    def __padding_op(self, Image):
+        if self.offset_x > 0:
+            padding_x = self.strides[0] - self.offset_x
         else:
-            dir_path = source_dir
-        
-        if im_path:
-            Image = cv2.imread(im_path)
-            Images = [Image]
+            padding_x = 0
+        if self.offset_y > 0:
+            padding_y = self.strides[1] - self.offset_y
         else:
-            Images = read_images(source_dir)
+            padding_y = 0
+        Padded_Image = np.zeros(shape=(Image.shape[0]+padding_x, Image.shape[1]+padding_y, Image.shape[2]),dtype=Image.dtype)
+        Padded_Image[padding_x//2:(padding_x//2)+(Image.shape[0]),padding_y//2:(padding_y//2)+Image.shape[1],:] = Image    
+        print(Padded_Image.shape)
+        return Padded_Image
 
-        im_size = image_size(Images[0])
-        num_images = len(Images)
-        transformed_images = dict()
-        Images = np.array(Images)
-        if PADDING:
+    def __convolution_op(self, Image):
+        start_x = 0
+        start_y = 0
+        end_x = Image.shape[0]
+        end_y = Image.shape[1]
+        n_rows = end_x//self.strides[0] + 1
+        n_columns = end_y//self.strides[1] + 1
+        small_images = []
+        for i in range(n_rows-1):
+            for j in range(n_columns-1):
+                new_start_x = start_x+i*self.strides[0]
+                new_start_y= start_y+j*self.strides[1]
+                small_images.append(Image[new_start_x:new_start_x+self.size[0],new_start_y:new_start_y+self.size[1],:])
+        return small_images
+
+    def transform(self):
+        
+        if not(os.path.exists(self.source)):
+            raise Exception("Path does not exist!")
             
-            padded_images = []
+        else:
+            if self.source and not(self.BATCH):
+                Image = plt.imread(self.source)
+                Images = [Image]
+            else: 
+                Images = self.__read_images()
+
+            im_size = Images[0].shape
+            num_images = len(Images)
+            transformed_images = dict()
+            Images = np.array(Images)
             
-            if strides[0]==None and strides[1]==None:
-                strides[0] = size[0]
-                strides[1] = size[1]
-                offset_x = Images.shape[1]%size[0]
-                offset_y = Images.shape[2]%size[1]
-                for Image in Images:
-                    Image_Padded = Padding_op(Image, strides, offset_x,offset_y)
-                    padded_images.append(Image_Padded)
+            if self.PADDING:
                 
-            elif strides[0]==None and strides[1]!=None:
-                strides[0] = size[0]
-                offset_x = Images.shape[1]%size[0]
-                if strides[1] <=Images.shape[2]:
-                    offset_y = Offset_op(Images.shape[2], size[1], strides[1])
+                padded_images = []
+
+                if self.strides[0]==None and self.strides[1]==None:
+                    self.strides[0] = self.size[0]
+                    self.strides[1] = self.size[1]
+                    self.offset_x = Images.shape[1]%self.size[0]
+                    self.offset_y = Images.shape[2]%self.size[1]
+                    padded_images = list(map(self.__padding_op, Images))
+                                         
+                elif self.strides[0]==None and self.strides[1]!=None:   
+                    self.strides[0] = self.size[0]
+                    self.offset_x = Images.shape[1]%self.size[0]
+                    if self.strides[1] <= Images.shape[2]:
+                        self.offset_y = self.__offset_op(Images.shape[2], self.size[1], self.strides[1])
+                    else:
+                        raise Exception("stride_y must be between {0} and {1}".format(1,Images.shape[2]))
+                    padded_images = list(map(self.__padding_op, Images))
+
+                elif self.strides[0]!=None and self.strides[1]==None:   
+                    self.strides[1] = self.size[1]
+                    self.offset_y = Images.shape[2]%self.size[1]
+                    if self.strides[0] <=Images.shape[1]:
+                        self.offset_x = self.__offset_op(Images.shape[1], self.size[0], self.strides[0])
+                    else:
+                        raise Exception("stride_x must be between {0} and {1}".format(1,Images.shape[1]))
+                    padded_images = list(map(self.__padding_op, Images))
+                                         
                 else:
-                    print("stride_y must be between {0} and {1}".format(1,Images.shape[2]-size[1]))
+                    if self.strides[0] > Images.shape[1]:
+                        raise Exception("stride_x must be between {0} and {1}".format(1,Images.shape[1]))
                     
-                for Image in Images:
-                    Image_Padded = Padding_op(Image, strides, offset_x,offset_y)
-                    padded_images.append(Image_Padded)
-                    
-            elif strides[0]!=None and strides[1]==None:
-                
-                strides[1] = size[1]
-                offset_y = Images.shape[2]%size[1]
-                
-                if strides[0] <=Images.shape[1]:
-                    offset_x = Offset_op(Images.shape[1], size[0], strides[0])
-                else:
-                    print("stride_x must be between {0} and {1}".format(1,Images.shape[1]-size[0]))
-                
-                for Image in Images:
-                    Image_Padded = Padding_op(Image, strides, offset_x,offset_y)
-                    padded_images.append(Image_Padded)
+                    elif self.strides[1] > Images.shape[2]:
+                        raise Exception("stride_y must be between {0} and {1}".format(1,Images.shape[2]))
+                        
+                    else:
+                        self.offset_x = self.__offset_op(Images.shape[1], self.size[0], self.strides[0])
+                        self.offset_y = self.__offset_op(Images.shape[2], self.size[1], self.strides[1])
+                        padded_images = list(map(self.__padding_op, Images))
+
+                for i, Image in enumerate(padded_images):
+                    transformed_images[str(i)] = self.__convolution_op(Image)
+
             else:
-                if strides[0] > Images.shape[1] or strides[1] > Images.shape[2]:
-                    print("stride_x must be between {0} and {1} and stride_y must be between {2} and {3}".format(1,Images.shape[1]-size[0],1,Images.shape[2]-size[1] ))
-                
-                else:
-                    offset_x = Offset_op(Images.shape[1], size[0], strides[0])
-                    offset_y = Offset_op(Images.shape[2], size[1], strides[1])
-                    
-                for Image in Images:
-                    Image_Padded = Padding_op(Image, strides, offset_x,offset_y)
-                    padded_images.append(Image_Padded)
-            
-            count=0
-            for Image in padded_images:
-                count+=1
-                transformed_images[str(count)] = Convolution_op(Image,size,strides)
-        
-        else:
-            if strides[0]==None and strides[1]==None:
-                strides[0] = size[0]
-                strides[1] = size[1]
-                
-            elif strides[0]==None and strides[1]!=None:
-                strides[0] = size[0]
-                
-            elif strides[0]!=None and strides[1]==None:
-                strides[1] = size[1]
-            
-            count=0
-            for Image in Images:
-                count+=1
-                transformed_images[str(count)] = Convolution_op(Image,size,strides)
-        
-        return transformed_images
+                if self.strides[0]==None and self.strides[1]==None:
+                    self.strides[0] = self.size[0]
+                    self.strides[1] = self.size[1]
 
+                elif self.strides[0]==None and self.strides[1]!=None:
+                    self.strides[0] = self.size[0]
+
+                elif self.strides[0]!=None and self.strides[1]==None:
+                    self.strides[1] = self.size[1]
+                                         
+                for i, Image in enumerate(Images):
+                    transformed_images[str(i)] = self.__convolution_op(Image)
+
+            return transformed_images
+        
+    def save_images(self,transformed, save_dir):
+        if not(os.path.exists(save_dir)):
+            raise Exception("Path does not exist!")
+        else:
+            for key, val in transformed.items():
+                path = os.path.join(save_dir, key)
+                os.mkdir(path)
+                for i, j in enumerate(val):
+                    plt.imsave(os.path.join(path, str(i+1)+'.png'), j)
